48fbf44d905bb3d7c0482fc4a98858c9ca3587b8
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-02-17 17:39:07 -0600

    Update modeling1.sql

--- modeling1.sql
+++ modeling1.sql
@@ -1,8 +1,8 @@
 CREATE VIEW orders_by_pizza_types AS
 
-SELECT state, type, total_number_of_pizzas_sold, gross_sales, (gross_sales / population) AS gross_sales_per_capita, number_of_unique_customers
+SELECT state, type, total_number_of_pizzas_sold, gross_sales_USD, (gross_sales_USD / population) AS gross_sales_USD_per_capita, number_of_unique_customers
 FROM (
-  SELECT state, type, sum(qty) AS total_number_of_pizzas_sold, sum(retail_price) AS gross_sales, count(DISTINCT customer_id) AS number_of_unique_customers
+  SELECT state, type, sum(qty) AS total_number_of_pizzas_sold, sum(retail_price) AS gross_sales_USD, count(DISTINCT customer_id) AS number_of_unique_customers
   FROM orders o
   LEFT JOIN customers c USING (customer_id)
   WHERE o.order_date > now() - INTERVAL 12 month
@@ -20,14 +20,14 @@ ORDER BY total_number_of_pizzas_sold DESC
 LIMIT 3
 
 -- Gross sales over the last 12 months for each type
-SELECT state, type, gross_sales
+SELECT state, type, gross_sales_USD
 FROM orders_by_pizza_types
 WHERE state = {}
 ORDER BY total_number_of_pizzas_sold DESC
 LIMIT 3
 
 -- Gross sales per capita over the last 12 months for each type
-SELECT state, type, gross_sales_per_capita
+SELECT state, type, gross_sales_USD_per_capita
 FROM orders_by_pizza_types
 WHERE state = {}
 ORDER BY total_number_of_pizzas_sold DESC
